7fe656fec7de038b7395f538ac3e3da37dad3a00
Alexey Adadurov <alexei.adadurov@mail.ru>
2020-02-02 23:49:39 +0300

    change enumeration stability test attempts to 10000

--- test/PskOnline.X20.Protocol.WinUSB.Test/Stability/Enumeration_Stability_Test.cs
+++ test/PskOnline.X20.Protocol.WinUSB.Test/Stability/Enumeration_Stability_Test.cs
@@ -29,13 +29,34 @@
     public void Init_Retry_1000()
     {
       var fac = SerilogHelper.GetLoggerFactory();
+      var fwRevision = "";
+      var fwBuildDate = "";
 
-      for( int i = 0; i < 1000; ++i)
+      for ( int i = 0; i < 10000; ++i)
       {
-        using (var device = DeviceHelper.GetFirstSuitableDevice(fac))
+        try
         {
-          var serialNumber = device.GetCapabilities().SerialNumber;
-          _logger.LogInformation($"Enumerated device {serialNumber}, attempt {i}");
+          using (var device = DeviceHelper.GetFirstSuitableDevice(fac))
+          {
+            var caps = device.GetCapabilities();
+            var serialNumber = caps.SerialNumber;
+            
+            if (string.IsNullOrEmpty(fwRevision))
+            {
+              fwRevision = caps.RevisionInfo;
+            }
+            if (string.IsNullOrEmpty(fwBuildDate))
+            {
+              fwBuildDate = caps.FirmwareBuildDate;
+            }
+          }
+        }
+        catch
+        {
+          _logger.LogInformation($"Failed after {i} successful attempts");
+          _logger.LogInformation($"Last FW revision was:   {fwRevision}");
+          _logger.LogInformation($"Last FW build date was: {fwBuildDate}");
+          throw;
         }
       }
     }
